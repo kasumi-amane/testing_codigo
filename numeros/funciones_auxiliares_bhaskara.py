1d34b87fdb658a8bd5292d9bc30258e4332dd6df
CaidevOficial <ffalcone@fra.utn.edu.ar>
2024-09-02 21:21:58 -0300

    primera subida de archivos de ejemplos de la practica de Python

---
+++ numeros/funciones_auxiliares_bhaskara.py
@@ -0,0 +1,7 @@
+from .funciones_auxiliares_primo import es_multiplo
+
+
+def validar_si_es_multiplo(numero_1, numero_2):
+    es_multiplo_valido = es_multiplo(numero_1, numero_2)
+    return es_multiplo_valido
+
